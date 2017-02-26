ed7a1b54b4103e1194b5d133ee0f545e54625033
Lianyu Ma <LianyuMa@users.noreply.github.com>
2017-02-26 17:19:10 -0500

    initialize

---
+++ sw.js
@@ -0,0 +1,11 @@
+'use strict';
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
