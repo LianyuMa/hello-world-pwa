becd23823ffd2a05d90f0a07a2c3e636f9852ec1
LianyuMa <mlyoct24@gmail.com>
2017-02-28 15:31:48 -0500

    polyfill

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
 'use strict';
 
+importScripts('/cache-polyfill.js');
+
 self.addEventListener('notificationclick', function(event) {
   console.log('[Service Worker] Notification click Received.');
 
@@ -7,5 +9,18 @@ self.addEventListener('notificationclick', function(event) {
 
   event.waitUntil(
     clients.openWindow('https://github.com/LianyuMa/hello-world-pwa')
+  );
+});
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('helloworldpwa').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.html?homescreen=1',
+        '/?homescreen=1',
+      ]);
+    })
   );
 });
