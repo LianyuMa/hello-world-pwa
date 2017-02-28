1c183573283c839b1270ff6f2c69325502bf2065
LianyuMa <mlyoct24@gmail.com>
2017-02-28 16:02:56 -0500

    fetch event listener

--- sw.js
+++ sw.js
@@ -9,3 +9,13 @@ self.addEventListener('notificationclick', function(event) {
     clients.openWindow('https://github.com/LianyuMa/hello-world-pwa')
   );
 });
+
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
