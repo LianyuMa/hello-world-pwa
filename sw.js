a53db5d39a9aa463e6d3546cbcdaeca77d78eb5d
Alex <mlyoct24@gmail.com>
2017-02-28 15:53:47 -0500

    Revert "polyfill"
    This reverts commit becd23823ffd2a05d90f0a07a2c3e636f9852ec1.

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 'use strict';
 
-importScripts('/cache-polyfill.js');
-
 self.addEventListener('notificationclick', function(event) {
   console.log('[Service Worker] Notification click Received.');
 
@@ -9,18 +7,5 @@ self.addEventListener('notificationclick', function(event) {
 
   event.waitUntil(
     clients.openWindow('https://github.com/LianyuMa/hello-world-pwa')
-  );
-});
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('helloworldpwa').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/index.html?homescreen=1',
-        '/?homescreen=1',
-      ]);
-    })
   );
 });
